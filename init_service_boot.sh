33af83332b04533470e7b7cbb8946c176a1c51ec
lukilme <kilmerluisluk12@gmail.com>
2024-11-06 00:08:38 -0300

    :moon: lua configs

--- init_service_boot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Service automatic boot system:"
-systemctl list-unit-files --type=service | grep enabled
--- memory_optimization.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SSD_DISKS=$(lsblk -d -o name,rota | grep -E '0$' | awk '{print $1}')
-for disk in $SSD_DISKS; do
-  echo "Otimizando /dev/$disk no fstab..."
-  echo "UUID=$(blkid -s UUID -o value /dev/$disk) /mnt ext4 defaults,noatime,discard 0 1" | sudo tee -a /etc/fstab
-done
-
-echo "Optimized file system!"
